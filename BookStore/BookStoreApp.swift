4f81456e771e7707b189d21eb27e174807b6782a
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-29 17:52:15 -0400

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Enrique Sarmiento on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookStore
+//
+//  Created by Enrique Sarmiento on 29/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
